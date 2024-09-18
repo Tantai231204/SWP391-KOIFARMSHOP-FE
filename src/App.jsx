64dc9c021dbd1f6629d4cdca76bf460820da3010
Tantai231204 <taintse184659@fpt.edu.vn>
2024-09-18 17:10:19 +0700

    hey

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+App.propTypes = {
+    
+};
+function App(props) {
+    return (
+        <div>
+        Hello
+        </div>
+    );
+}
+
+export default App
---
+++ src/components/features/Register/index.jsx
@@ -0,0 +1,16 @@
+
+import PropTypes from 'prop-types';
+
+Register.propTypes = {
+    Register: PropTypes.func,
+};
+
+function Register() {
+    return (
+        <div>
+            Register
+        </div>
+    );
+}
+
+export default Register;
