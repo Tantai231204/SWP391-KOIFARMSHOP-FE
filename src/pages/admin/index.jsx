09f6e3624367be1ca39361a58359acf74afd0374
Tantai231204 <taintse184659@fpt.edu.vn>
2024-09-22 01:01:26 +0700

    Fix form

--- src/pages/admin/index.jsx
+++ src/pages/admin/index.jsx
@@ -1,74 +1,52 @@
 import { Button } from "antd";
 import "./admin.scss";
+import logo from "/src/img/logo.svg";
+import  Header  from "/src/components/headers/index";
 import { Container } from "react-bootstrap";
-
-
+import { NavLink } from "react-router-dom";
 function Admin() {
   return (
-    <Container>
-    <div className="admin-container">
-      <div className="admin-title"> Quản lý </div>
-      <div className="button-manage">
-        <div>
-          <Button
-            icon={
-              <img
-                src="/src/img/logo.svg"
-                alt="logo"
-                style={{ width: 61, height: 59 }}
-              />
-            }
-          >
-            Hồ sơ khách hàng
-          </Button>
-          <Button
-            icon={
-              <img
-                src="/src/img/logo.svg"
-                alt="logo"
-                style={{ width: 61, height: 59 }}
-              />
-            }
-          >
-            Hồ sơ khách hàng
-          </Button>
+    <>
+      <Header />
+      <Container>
+      <div className="admin-container">
+        <div className="admin-title"> Quản lý </div>
+        <div className="button-manage">
+          <div>
+            <Button
+              icon={<img src={logo} alt="logo" style={{ width: "24px" }} />}
+            >
+              <NavLink to="/">Hồ sơ khách hàng</NavLink>
+            </Button> 
+            <Button
+              icon={<img src={logo} alt="logo" style={{ width: "24px" }} />}
+            >
+              <NavLink> Quản lý ký gửi</NavLink>
+            </Button>
+          </div>
+          <div>
+            <Button
+              icon={<img src={logo} alt="logo" style={{ width: "24px" }} />}
+            >
+              <NavLink>Quản lý giống cá</NavLink>
+            </Button>
+            <Button
+              icon={<img src={logo} alt="logo" style={{ width: "24px" }} />}
+            >
+            <NavLink>Quản lý đơn hàng</NavLink>
+            </Button>
+          </div>
         </div>
-        <div>
+        <div className="button-last">
           <Button
-            icon={
-              <img
-                src="/src/img/logo.svg"
-                alt="logo"
-                style={{ width: 61, height: 59 }}
-              />
-            }
+            icon={<img src={logo} alt="logo" style={{ width: "24px" }} />}
           >
-            Hồ sơ khách hàng
-          </Button>
-          <Button
-            icon={
-              <img
-                src="/src/img/logo.svg"
-                alt="logo"
-                style={{ width: 61, height: 59 }}
-              />
-            }
-          >
-            Hồ sơ khách hàng
+            <NavLink>Thống kê và báo cáo</NavLink>
           </Button>
         </div>
       </div>
-      <div className="button-last">
-        <Button
-          icon={
-            <img src="/src/img/logo.svg" alt="logo" style={{ width: 61, height: 59 }} />
-          }
-        >
-          Hồ sơ khách hàng
-        </Button>
-      </div>
-    </div>
-    </Container>
+      </Container>
+    </>
   );
 }
 
